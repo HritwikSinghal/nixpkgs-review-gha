5bd017db80f6be8b496da8aa8f0f9b7c7638ba24
Defelo <mail@defelo.de>
2025-07-16 17:30:50 +0200

    feat: add commit hash to report

---
+++ packages.nix
@@ -0,0 +1,8 @@
+let
+  nixpkgs = builtins.getFlake "github:nixos/nixpkgs/nixpkgs-unstable";
+  nixpkgs-review = builtins.getFlake "github:Mic92/nixpkgs-review/d9ab24795df1bf1d8374d3f9606f3416596b821b";
+in
+
+import nixpkgs {
+  overlays = [ (final: prev: { inherit (nixpkgs-review.packages.${final.system}) nixpkgs-review; }) ];
+}
