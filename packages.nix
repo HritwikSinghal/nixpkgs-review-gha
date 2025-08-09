e2a68fe39760026754a8f0ef2cc3336395498b49
Defelo <mail@defelo.de>
2025-08-09 22:38:21 +0200

    feat: add section for still failing packages to report

--- packages.nix
+++ packages.nix
@@ -4,5 +4,15 @@ let
 in
 
 import nixpkgs {
-  overlays = [ (final: prev: { inherit (nixpkgs-review.packages.${final.system}) nixpkgs-review; }) ];
+  overlays = [
+    (final: prev: {
+      inherit (nixpkgs-review.packages.${final.system}) nixpkgs-review;
+
+      generate-markdown-report = final.writers.writePython3Bin "generate-markdown-report" {
+        flakeIgnore = [
+          "E501" # line too long
+        ];
+      } (builtins.readFile ./generate_markdown_report.py);
+    })
+  ];
 }
